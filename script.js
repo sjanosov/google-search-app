4e46834977a1fcdade9a56db660d09cc87e2eac5
Simona Janosova <4844982-SimonaJ@users.noreply.gitlab.com>
2020-05-03 21:43:04 +0200

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const imagesDiv = document.querySelector("#imagesDiv");
 const textsDiv = document.querySelector("#textsDiv");
-const searchButton = document.querySelector(".search-button");
+const btnSearch = document.querySelector("#btnSearch");
 const searchInput = document.querySelector(".search-input");
+const contentContainer = document.querySelector(".content-container");
+const btnPrevious = document.querySelector("#btnPrevious");
+const btnNext = document.querySelector("#btnNext");
+const pageNumber = document.querySelector("#pageNumber");
+
+contentContainer.style = "display: none";
+btnPrevious.style = "display: none";
+btnNext.style = "display: none";
+let startIndex = 1;
+let lastQuery;
+let apiKey = "AIzaSyCCI0VqrPeTIBNU6Z_hNnyrLOTjufkTpAc";
+let customSearchId = "001070535140200413681:8sbflscrdn2";
 
 async function search(query) {
   if (query === "" || query === null || query === undefined) {
@@ -10,15 +22,48 @@ async function search(query) {
   }
   let texts = await getTexts(query);
   let images = await getImages(query);
+  let nothingFound = texts == undefined && images == undefined;
+  if (nothingFound) {
+    contentContainer.style = "display: none";
+    btnPrevious.style = "display: none";
+    btnNext.style = "display: none";
 
+    return;
+  } else {
+    contentContainer.style = "display: flex";
+  }
   appendTexts(texts);
   appendImages(images);
 }
+
 function onSearchClick(e) {
   e.preventDefault();
-  search(searchInput.value);
+  let doCallout = true;
+
+  if (e.target.id == btnSearch.id) {
+    lastQuery = searchInput.value;
+    startIndex = 1;
+  } else if (lastQuery != searchInput.value) {
+    doCallout = false;
+  }
+  pageNumber.innerHTML = (startIndex - 1) / 10 + 1;
+
+  if (startIndex == 1) {
+    btnPrevious.style = "visibility: hidden";
+  } else {
+    btnPrevious.style = "visibility: visible";
+  }
+  if (startIndex == 91) {
+    btnNext.style = "visibility: hidden";
+  } else {
+    btnNext.style = "visibility: visible";
+  }
+
+  if (doCallout) {
+    search(searchInput.value);
+  }
 }
-searchButton.addEventListener("click", onSearchClick);
+btnSearch.addEventListener("click", onSearchClick);
 
 function appendImages(imageItems) {
   imagesDiv.innerHTML = "";
@@ -26,15 +71,24 @@ function appendImages(imageItems) {
     console.log(imageItem);
     imgDiv = document.createElement("div");
     imgDiv.classList.add("image-div");
-    imgDiv.innerHTML = `<img src=${imageItem.link}></img>`;
+    if (imageItem.fileFormat == "image/") {
+      imgDiv.innerHTML = `<a target="_blank" href=${imageItem.link}><img src=${imageItem.image.thumbnailLink}></img></a>`;
+    } else {
+      imgDiv.innerHTML = `<a target="_blank" href=${imageItem.link}><img src=${imageItem.link}></img></a>`;
+    }
     imagesDiv.appendChild(imgDiv);
   });
 }
 
 async function getImages(query) {
-  const dataFetch = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=AIzaSyAzyZzUoVr7f9bT3I2w670FU4I5JawGIsw&cx=013951971749308929015:shiobss27n3&q=${query}&searchType=image`
-  );
+  let tmpUrl = new URL("https://www.googleapis.com/customsearch/v1");
+  tmpUrl.searchParams.append("key", apiKey);
+  tmpUrl.searchParams.append("cx", customSearchId);
+  tmpUrl.searchParams.append("q", query);
+  tmpUrl.searchParams.append("searchType", "image");
+  tmpUrl.searchParams.append("start", startIndex);
+
+  const dataFetch = await fetch(tmpUrl);
   const data = await dataFetch.json();
   return data.items;
 }
@@ -44,18 +98,31 @@ function appendTexts(textItems) {
   textItems.forEach((textItem) => {
     let textDiv = document.createElement("div");
     textDiv.classList.add("text-div");
-    textDiv.innerHTML = `<p>${textItem.htmlTitle}</p><a href="${textItem.link}">${textItem.link}</a>`;
+    textDiv.innerHTML = `<p>${textItem.htmlTitle}</p><a target="_blank" href="${textItem.link}">${textItem.link}</a>`;
     textsDiv.appendChild(textDiv);
   });
 }
 
 async function getTexts(query) {
-  console.log(query);
-
-  const dataFetch = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=AIzaSyAzyZzUoVr7f9bT3I2w670FU4I5JawGIsw&cx=013951971749308929015:shiobss27n3&q=${query}&sort=date`
-  );
+  let tmpUrl = new URL("https://www.googleapis.com/customsearch/v1");
+  tmpUrl.searchParams.append("key", apiKey);
+  tmpUrl.searchParams.append("cx", customSearchId);
+  tmpUrl.searchParams.append("q", query);
+  tmpUrl.searchParams.append("start", startIndex);
+  const dataFetch = await fetch(tmpUrl);
   const data = await dataFetch.json();
   return data.items;
 }
-getTexts();
+
+function decreasePage(e) {
+  startIndex = startIndex - 10;
+  onSearchClick(e);
+}
+
+function increasePage(e) {
+  startIndex = startIndex + 10;
+  onSearchClick(e);
+}
+
+btnPrevious.addEventListener("click", decreasePage);
+btnNext.addEventListener("click", increasePage);

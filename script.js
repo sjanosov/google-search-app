45ed9e934ac147ae5b437c855f1d94f6b985c188
Simona Janosova <4844982-SimonaJ@users.noreply.gitlab.com>
2020-05-04 21:31:08 +0200

    final commit

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ const textsDiv = document.querySelector("#textsDiv");
 const btnSearch = document.querySelector("#btnSearch");
 const searchInput = document.querySelector(".search-input");
 const contentContainer = document.querySelector(".content-container");
-const btnPrevious = document.querySelector("#btnPrevious");
-const btnNext = document.querySelector("#btnNext");
-const pageNumber = document.querySelector("#pageNumber");
+const btnPreviousArray = document.querySelectorAll(".btn-previous");
+const btnNextArray = document.querySelectorAll(".btn-next");
+const btnContainerDesktop = document.querySelector("#btnContainerDesktop");
+const btnContainerMobile = document.querySelector("#btnContainerMobile");
+const pageNumberArray = document.querySelectorAll(".pageNumber");
+const back2Top = document.querySelector("#back2Top");
+const btnImage = document.querySelector(".image-button");
+const notFound = document.querySelector(".not-found");
 
 contentContainer.style = "display: none";
-btnPrevious.style = "display: none";
-btnNext.style = "display: none";
+btnContainerDesktop.style = "display: none";
+btnContainerMobile.style = "display: none";
+
+btnImage.style = "visibility: hidden";
+notFound.style = "display: none";
+
 let startIndex = 1;
 let lastQuery;
-let apiKey = "AIzaSyCCI0VqrPeTIBNU6Z_hNnyrLOTjufkTpAc";
+let apiKey = "AIzaSyDs_oxKNzPlM6SusPYEvYp_Im0nOy797Vg";
 let customSearchId = "001070535140200413681:8sbflscrdn2";
 
 async function search(query) {
   if (query === "" || query === null || query === undefined) {
-    console.log("som tu");
     return;
   }
   let texts = await getTexts(query);
@@ -25,19 +33,45 @@ async function search(query) {
   let nothingFound = texts == undefined && images == undefined;
   if (nothingFound) {
     contentContainer.style = "display: none";
-    btnPrevious.style = "display: none";
-    btnNext.style = "display: none";
+    btnImage.style = "visibility: hidden";
+    btnContainerDesktop.style = "visibility: hidden";
+    btnContainerMobile.style = "visibility: hidden";
+    btnContainerDesktop.style = "visibility: hidden";
+    btnContainerMobile.style = "visibility: hidden";
+    notFound.style = "display: block";
 
     return;
   } else {
     contentContainer.style = "display: flex";
+    btnImage.style = "visibility: visible";
+    btnContainerDesktop.style = "visibility: visible";
+    btnContainerMobile.style = "visibility: visible";
+    btnContainerDesktop.style = "visibility: visible";
+    btnContainerMobile.style = "visibility: visible";
+    notFound.style = "display: none";
   }
   appendTexts(texts);
   appendImages(images);
+
+  //page number count
+  pageNumberArray[0].innerHTML = (startIndex - 1) / 10 + 1;
+  pageNumberArray[1].innerHTML = (startIndex - 1) / 10 + 1;
+
+  if (startIndex == 1) {
+    btnPreviousArray.style = "visibility: hidden";
+  } else {
+    btnPreviousArray.style = "visibility: visible";
+  }
+  if (startIndex == 91) {
+    btnNextArray.style = "visibility: hidden";
+  } else {
+    btnNextArray.style = "visibility: visible";
+  }
 }
 
 function onSearchClick(e) {
   e.preventDefault();
+  //do call only if btnSearch is clicked, not if input is retyped
   let doCallout = true;
 
   if (e.target.id == btnSearch.id) {
@@ -45,18 +79,6 @@ function onSearchClick(e) {
     startIndex = 1;
   } else if (lastQuery != searchInput.value) {
     doCallout = false;
-  }
-  pageNumber.innerHTML = (startIndex - 1) / 10 + 1;
-
-  if (startIndex == 1) {
-    btnPrevious.style = "visibility: hidden";
-  } else {
-    btnPrevious.style = "visibility: visible";
-  }
-  if (startIndex == 91) {
-    btnNext.style = "visibility: hidden";
-  } else {
-    btnNext.style = "visibility: visible";
   }
 
   if (doCallout) {
@@ -68,7 +90,6 @@ btnSearch.addEventListener("click", onSearchClick);
 function appendImages(imageItems) {
   imagesDiv.innerHTML = "";
   imageItems.forEach((imageItem) => {
-    console.log(imageItem);
     imgDiv = document.createElement("div");
     imgDiv.classList.add("image-div");
     if (imageItem.fileFormat == "image/") {
@@ -88,8 +109,15 @@ async function getImages(query) {
   tmpUrl.searchParams.append("searchType", "image");
   tmpUrl.searchParams.append("start", startIndex);
 
-  const dataFetch = await fetch(tmpUrl);
-  const data = await dataFetch.json();
+  const data = await fetch(tmpUrl)
+    .then((response) => {
+      if (response.ok) return response.json();
+      else throw Error(`Request rejected with status ${response.status}.`);
+    })
+    .catch((error) => {
+      notFound.innerHTML = error.message;
+      return {};
+    });
   return data.items;
 }
 
@@ -109,20 +137,58 @@ async function getTexts(query) {
   tmpUrl.searchParams.append("cx", customSearchId);
   tmpUrl.searchParams.append("q", query);
   tmpUrl.searchParams.append("start", startIndex);
-  const dataFetch = await fetch(tmpUrl);
-  const data = await dataFetch.json();
+  const data = await fetch(tmpUrl)
+    .then((response) => {
+      if (response.ok) return response.json();
+      else throw Error(`Request rejected with status ${response.status}.`);
+    })
+    .catch((error) => {
+      notFound.innerHTML = error.message;
+      return {};
+    });
   return data.items;
 }
 
 function decreasePage(e) {
-  startIndex = startIndex - 10;
+  if (startIndex > 1) {
+    startIndex = startIndex - 10;
+  }
+
   onSearchClick(e);
 }
 
 function increasePage(e) {
-  startIndex = startIndex + 10;
+  if (startIndex < 91) {
+    startIndex = startIndex + 10;
+  }
   onSearchClick(e);
 }
 
-btnPrevious.addEventListener("click", decreasePage);
-btnNext.addEventListener("click", increasePage);
+//when scrolled down 20 px from the top, show button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    back2Top.style.display = "block";
+  } else {
+    back2Top.style.display = "none";
+  }
+}
+
+// When clicked on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: "smooth" }); //working on mobile chrome
+}
+function downFunction() {
+  let imgPosition = imagesDiv.offsetTop;
+
+  window.scrollTo({ top: imgPosition, behavior: "smooth" }); //working on mobile chrome
+}
+
+btnPreviousArray[0].addEventListener("click", decreasePage);
+btnPreviousArray[1].addEventListener("click", decreasePage);
+
+btnNextArray[0].addEventListener("click", increasePage);
+btnNextArray[1].addEventListener("click", increasePage);
